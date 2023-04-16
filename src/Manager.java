a7cdc143cbffaa838f26c74b34892480e3d0d8f2
Chernosmaga <sergeychernosmaga1997@yandex.ru>
2023-04-16 13:34:22 +0300

    feat: добавил метод для считывания слов из файла

--- src/Manager.java
+++ src/Manager.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Manager {
 
-    private HashMap<String, Character> wordsMap; // map with words, where key is a word and values are letters
+    private HashMap<String, Character> wordsMap; // мапа слов, где ключ - слово, значения - буквы
 
     public Manager() {
         this.wordsMap = new HashMap<>();
@@ -20,36 +20,50 @@ public class Manager {
         this.wordsMap = wordsMap;
     }
 
+    public void readWords() { // метод для считывания слов из файла
+        List<String> content = readFileContents("resources/Words");
+        for (int i = 0; i < content.size(); i++) { // разделяю на составные части
+            String[] words = content.get(i).split(","); // делю по запятой
+            for (int j = 0; j < words.length; j++) { // достаю слова из массива
+                String word = words[j]; // получаю отдельное слово
+                for (int k = 0; k < word.length(); k++) { // делю слова на буквы
+                    char letters = (char) k; // привожу к типу char
+                    wordsMap.put(word, letters); // кладу в мапу слова и буквы
+                }
+            }
+        }
+    }
+
     public void addWord(String word) {
-        if (word.length() <= 8) { // the word mustn't be more than six characters
-            if (wordsMap.containsKey(word)) { // checking if there's already similar word in a map
-                System.out.println("Are you a cheater or something?\nWe've already got the same word");
+        if (word.length() <= 8) { // слово должно быть не больше восьми элементов
+            if (wordsMap.containsKey(word)) { // проверка есть ли подобное слово в мапе
+                System.out.println("Ты типо читер?\nТакое слово уже есть в списке");
             } else {
                 char[] wordToArray = word.toCharArray();
                 for (int i = 0; i < wordToArray.length; i++) {
-                    char letters = (char) i; // casting to char type
-                    wordsMap.put(word, letters); // adding values into a map
+                    char letters = (char) i; // привожу к типу char
+                    wordsMap.put(word, letters); // добавляю значения в мапу
                 }
-                System.out.println("All done! The word has gone!");
+                System.out.println("Готово!");
             }
         } else {
-            System.out.println("Word must contain only six letters!");
+            System.out.println("Слово должно быть не больше восьми букв!");
         }
     }
 
     public void printHowToPlay() {
         List<String> content = readFileContents("resources/Document.txt");
         for (int i = 0; i < content.size(); i++) {
-            String[] sentences = content.get(i).split("\n"); // splitting a text file into sentences
-            System.out.println(Arrays.toString(sentences)); // printing sentences
+            String[] sentences = content.get(i).split("\n"); // разделяю файл на предложения
+            System.out.println(Arrays.toString(sentences)); // печать предложений
         }
     }
 
     public void printAnIDEA() {
-        // there should be a link to some stupid video on YouTube
+        // здесь любая реализация метода, чисто по рофлу
     }
 
-    List<String> readFileContents(String path) { // there's a method to read files
+    List<String> readFileContents(String path) { // метод для считывания файлов
         try {
             return Files.readAllLines(Path.of(path));
         } catch (IOException e) {
