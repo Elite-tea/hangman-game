3b8f96d62aa73457171a3af2ea0657b51e412452
Chernosmaga <sergeychernosmaga1997@yandex.ru>
2023-04-19 19:16:53 +0300

    feat: изменил массив на лист

--- src/Game.java
+++ src/Game.java
@@ -5,25 +5,30 @@ public class Game {
 
     static Manager manager = new Manager();
     static Random generator = new Random();
-    static char[] letters; // попробовать вместо массива использовать лист String
+    static List<Character> letters; // попробовать вместо массива использовать лист String
 
     public void guessingALetter(char letter) {
-        char[] localArray = new char[8];
-
-        for (int i = 0; i < letters.length; i++) {
-            if (letters.equals(letter)) {
-                localArray[i] =
+        for (Character searchedLetter : letters) {
+            if (searchedLetter.equals(letter)) { // если есть буква в слове, она должна стоять
+                // на месте вывода по определённому индексу, типо sout(__a__a)
             }
         }
     }
 
     public void printingAWord() {
+        manager.readWordsFile(); // вызываю метод, чтоб при запуске игры считались слова из файла
         List<String> localListOfWords = manager.getWordsList(); // создаю новый лист, чтоб сложить туда слова
         String randomWord = localListOfWords.get(generator.nextInt(localListOfWords.size()));
         // получаю рандомное слово из листа
-        letters = randomWord.toCharArray(); // разбиваю рандомное слово по буквам
-        char first = letters[0];
-        char fourth = letters[5];
+        // а л л е р г и я
+        if (letters == null) {
+            for (int i = 0; i < randomWord.length(); i++) {
+                char letter = randomWord.charAt(i);
+                letters.add(letter); // ЗДЕСЬ NULL POINTER EXCEPTION
+            } // разбиваю рандомное слово по буквам
+        }
+        char first = (char) letters.indexOf(0);
+        char fourth = (char) letters.indexOf(5);
         // 0  1  2  3  4  5  6  7
         System.out.println(first + "_____" + fourth + "__");
         // печатаю слово для того, чтоб пользователь начинал угадывать буквы
