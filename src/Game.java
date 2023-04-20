9e08263c082b76ae4eb4ede57688520530cc34fb
Kirill Andreev <Andreewkirill64@yandex.ru>
2023-04-20 12:55:52 +0400

    style: Код приведен к общему стандарту кода в Practicum fix: Исправлена ошибка, из-за которой не обновлялись слова, если продолжить играть после победы/поражения

--- src/Game.java
+++ src/Game.java
@@ -1,68 +1,71 @@
 import java.util.*;
 
 public class Game {
-
     static Manager manager = new Manager();
     static Random generator = new Random();
-    static List<String> letters = new ArrayList<>(); // попробовать вместо массива использовать лист String
+    static List<String> letters = new ArrayList<>();
     static HashMap<Integer,String> guessedLetters = new HashMap<>();
     Hangman hangman = new Hangman();
 
     public void guessingALetter(Scanner read) {
         System.out.println("Начнём?\n");
-        hangman.printHangmanZeroTry();
+        hangman.printHangmanZeroTry(); // печать пустой виселицы
         System.out.println("\nСлово: ");
         printingAWord();
         boolean found;
-        int counter = 6;
+        int tries = 6; // пользователю дано шесть попыток
         int counterLetter = 0;
 
         while (true) {
             found = false;
-            System.out.println("\nПодумай и введи букву, которая может быть в слове, начальные буквы могут повторяться");
+            System.out.println("\nПодумай и введи букву, которая может быть в слове");
             String letter = read.next(); // считываю букву, которая может быть в слове
+
             for (int i = 0; i < letters.size(); i++) {
-                if (letters.get(i).equals(letter)) { // если есть буква в слове, она должна стоять
-                    guessedLetters.put(i, letter);// на месте вывода по определённому индексу, подобие sout(__a__a)
+                if (letters.get(i).equals(letter)) { // если есть буква в слове, то заполняется HashMap угаданных букв
+                    guessedLetters.put(i, letter);
                     found = true;
-                    counterLetter++;
+                    counterLetter++; // инкрементируем счётчик букв в слове
                 }
             }
 
             for (int i = 0; i < letters.size(); i++) {
-                guessedLetters.putIfAbsent(i, "_"); // Заполняем hashmap шаблоном пустых букв
+                guessedLetters.putIfAbsent(i, "_"); // заполняем HashMap шаблоном пустых букв
             }
 
             System.out.println(guessedLetters.get(0) + guessedLetters.get(1) + guessedLetters.get(2) +
                     guessedLetters.get(3) + guessedLetters.get(4) + guessedLetters.get(5) +
-                    guessedLetters.get(6) + guessedLetters.get(7)); // Печать слова с шаблоном
+                    guessedLetters.get(6) + guessedLetters.get(7)); // печать слова с шаблоном
 
-            if (counterLetter == 6) { // Проверка на выигрыш, если выиграл, печатается сообщение
+            if (counterLetter == 6) { // проверка на выигрыш, если выиграл, печатается сообщение
                 System.out.println("Сегодня вешать некого :)");
+                letters.clear();
+                guessedLetters.clear();
                 break;
             } else {
                 if (!found) {// Проверка ошибок. Если ошибся - печатается одна из стадий виселицы
-                    counter--;
+                    tries--;
 
-                    if (counter == 5) {
+                    if (tries == 5) {
                         hangman.printHangmanFirstTry();
-                    } else if (counter == 4) {
+                    } else if (tries == 4) {
                         hangman.printHangmanSecondTry();
-                    } else if (counter == 3) {
+                    } else if (tries == 3) {
                         hangman.printHangmanThirdTry();
-                    } else if (counter == 2) {
+                    } else if (tries == 2) {
                         hangman.printHangmanForthTry();
-                    } else if (counter == 1) {
+                    } else if (tries == 1) {
                         hangman.printHangmanFifthTry();
-                    } else { // Проигрыш, игра окончена
+                    } else { // проигрыш, игра окончена
                         hangman.printHangmanSixthTry();
-                        System.out.println("Увы, сегодня повешены пиксели :(");
+                        letters.clear();
+                        guessedLetters.clear();
                         break;
                     }
                 }
             }
         }
-        }
+    }
 
     public void printingAWord() {
         manager.readWordsFile(); // вызываю метод, чтоб при запуске игры считались слова из файла
@@ -70,17 +73,18 @@ public class Game {
         // получаю случайное слово из листа
         String randomWord = localListOfWords.get(generator.nextInt(localListOfWords.size()));
 
-            for (int i = 0; i < randomWord.length(); i++) {
-                char letter = randomWord.charAt(i);
-                letters.add(String.valueOf(letter));
-            } // разбиваю случайное слово по буквам
+        for (int i = 0; i < randomWord.length(); i++) { // разбиваю случайное слово по буквам
+            char letter = randomWord.charAt(i);
+            letters.add(String.valueOf(letter));
+        }
 
-        String first = letters.get(0);
-        String fourth = letters.get(5);
+        String first = letters.get(0); // по задумке печатается элемент под нулевым и пятым индексами
+        String fourth = letters.get(5); // в дальнейшем планирую исправить, чтоб буквы тоже печатались рандомные
         guessedLetters.put(0,letters.get(0));
         guessedLetters.put(5,letters.get(5));
 
         // печатаю слово для того, чтоб пользователь начинал угадывать буквы
         System.out.println(first + "_____" + fourth + "__");
     }
+
 }
